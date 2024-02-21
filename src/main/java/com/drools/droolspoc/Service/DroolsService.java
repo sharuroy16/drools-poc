2ae40bc6fe9ce527255047bf8569f2ee7e4cea2e
Sharu Roy <54369231+sharuroy16@users.noreply.github.com>
2024-02-21 13:56:42 -0500

    Update DroolsService.java

--- src/main/java/com/drools/droolspoc/Service/DroolsService.java
+++ src/main/java/com/drools/droolspoc/Service/DroolsService.java
@@ -2,11 +2,8 @@ package com.drools.droolspoc.Service;
 
 import com.drools.droolspoc.Model.OrderDiscount;
 import com.drools.droolspoc.Model.OrderRequest;
-import com.drools.droolspoc.Model.RateRequest;
 
 public interface DroolsService {
     
     OrderDiscount getOrderDiscount(OrderRequest orderRequest);
-
-    String getInterestRate(RateRequest rateRequest);
 }
