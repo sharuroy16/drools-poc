ed8fa82cf30f2c7290439660b2b2b7da3b4e68cb
Sharu Roy <54369231+sharuroy16@users.noreply.github.com>
2024-02-21 13:56:20 -0500

    Update DroolsController.java

--- src/main/java/com/drools/droolspoc/Controller/DroolsController.java
+++ src/main/java/com/drools/droolspoc/Controller/DroolsController.java
@@ -1,11 +1,9 @@
 package com.drools.droolspoc.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.drools.droolspoc.Model.OrderDiscount;
@@ -23,12 +21,5 @@ public class DroolsController {
     @PostMapping(path = "/getOrderDiscount")
     public OrderDiscount getOrderDiscount(@RequestBody OrderRequest orderRequest){
         return droolsService.getOrderDiscount(orderRequest);
-    }
-
-    @GetMapping(path = "/getInterestRate")
-    public String getInterestRate(@RequestParam(required = true) String loanType,
-			@RequestParam(required = true) Integer durationInYear, @RequestParam(required = true) Integer age){
-        RateRequest rateRequest = new RateRequest(loanType, durationInYear, age);
-        return droolsService.getInterestRate(rateRequest);
     }
 }
